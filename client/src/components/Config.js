cbb828b987e5155673218f07b88742f89010a6db
nan-noo <54002105+nan-noo@users.noreply.github.com>
2021-07-29 22:07:14 +0900

    Update Config.js

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,5 +1,3 @@
 export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
-
-// movieapp api key: dca9126aba30040283877d9ac93e566f
