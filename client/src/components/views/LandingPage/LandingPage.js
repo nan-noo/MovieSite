59442b70100f7e7edb7aabb4d13a8915b74e31cd
nan-noo <54002105+nan-noo@users.noreply.github.com>
2021-07-29 22:54:41 +0900

    Main Image using TMDB API
    using TMDB API

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,23 +1,51 @@
-import {React, useEffect} from 'react'
-import axios from 'axios';
+import {React, useEffect, useState} from 'react'
+import { API_URL, IMAGE_BASE_URL } from '../../Config';
+import API_KEY from '../../../key';
+import MainImage from './Sections/MainImage';
 
 function LandingPage(props) {
+    const [Inputs, setInputs] = useState({
+        movies: [],
+        mainMovieImage: null 
+    });
+
+    const {movies, mainMovieImage} = Inputs;
+
     useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => console.log(response))
+        const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            //console.log(response);
+            setInputs({
+                ...Inputs,
+                movies: [response.results],
+                mainMovieImage: response.results[0]         
+            });
+        })
     }, []);
 
     return (
-        <div className="app"
-        style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-            height: '100vh'
-        }}>
-            <h2>Welcome!</h2>
+        <div style={{ width: '100%', margin: '0'}}>
+            {/* Main Image: 받아와야 rendering됨*/}
+            {mainMovieImage && 
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}/w1280${mainMovieImage.backdrop_path}`}
+                    title={mainMovieImage.original_title}
+                    desc={mainMovieImage.overview}
+                />
+            }
+            <div style={{width: '85%', margin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr/>
+                {/* Movie Grid Cards */}
+            </div>
+
+
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
         </div>
     );
 }
